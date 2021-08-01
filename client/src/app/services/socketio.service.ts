6e71fff32e8599725ae74c97e68ad922043538c1
Ramnarayan-Kotagiri <kotagiriramnarayan@gmail.com>
2021-08-01 18:15:39 +0530

    Updated environment import in socket service

--- client/src/app/services/socketio.service.ts
+++ client/src/app/services/socketio.service.ts
@@ -1,4 +1,4 @@
-import { environment } from './../environments/environment';
+import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
 import { io } from 'socket.io-client';
 import { Observable } from 'rxjs';
